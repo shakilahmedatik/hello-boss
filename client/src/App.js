e62658193de67c8e9de0f142dc6b73387f89a9e9
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-04-11 21:01:39 +0600

    Routing & Pages Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Profile from "./pages/Profile/Profile";
+import Register from "./pages/Authentication/Register";
+import Login from "./pages/Authentication/Login";
+import PassReset from "./pages/Authentication/PassReset";
+import Contact from "./pages/Contact/Contact";
+import About from "./pages/About/About";
+import NotFound from "./pages/NotFound/NotFound";
+
 const App = () => {
   return (
-    <div className="sweet-loading">
-      <h1>Employee Management System</h1>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/password-reset" element={<PassReset />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
---
+++ client/src/pages/Authentication/PassReset.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PassReset = () => {
+  return (
+    <div>
+      <h1>Password Reset Page</h1>
+    </div>
+  );
+};
+
+export default PassReset;
