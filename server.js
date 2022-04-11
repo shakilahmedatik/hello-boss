f6886670331645d1c5d7c1998e06ba3f79a8d111
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-04-11 16:20:52 +0600

    initial server setup done

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+const morgan = require("morgan");
+require("dotenv").config();
 
+//Express App
+const app = express();
+
+// Middleware
+app.use(express.json({ limit: "5mb" }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+
+
+
+// port
+const port = process.env.PORT || 8000
+
+app.listen(port, () => console.log(`Server is running on port ${port}`))
