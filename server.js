60027db35934dedf9ed8fe706c1ddc216c30d990
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-05-06 17:07:48 +0600

    server updated now

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { readdirSync } from 'fs'
+import { readdirSync } from "fs";
 import cookieParser from "cookie-parser";
 const morgan = require("morgan");
-import './database/db'
+import "./database/db";
 require("dotenv").config();
 
+// New comment in server
 
 //Express App
 const app = express();
@@ -15,19 +16,18 @@ app.use(cookieParser());
 app.use(morgan("dev"));
 
 // routes
-readdirSync('./routes').map(r => app.use('/api', require(`./routes/${r}`)))
+readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
 // Production Settings
-const path = require('path')
-if(process.env.NODE_ENV==='production'){
-    app.use('/', express.static('client/build'))
-    app.get('*', (req, res)=>{
-        res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
-    })
+const path = require("path");
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+  });
 }
 
 // port
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`))
-
+app.listen(port, () => console.log(`Server is running on port ${port}`));
